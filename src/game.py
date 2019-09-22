b931b0a8ae6f99d8bc44fc0df862ac6867d3bc08
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-09-21 20:51:16 -0400

    initial commit for game.py

---
+++ src/game.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+
+def new_game(surface):
+    pass
+
+
+def continue_game(surface):
+    pass
+
+
+def quit_game(surface):
+    from pygame import quit
+    from sys import exit
+
+    quit()
+    exit()
