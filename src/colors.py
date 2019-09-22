09cf7139d9988cb51e045d1827dfdf62697637f8
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-09-21 22:51:42 -0400

    add tower information on colors and bmp

--- src/colors.py
+++ src/colors.py
@@ -5,3 +5,15 @@ from pygame import Color
 
 black = Color(20, 20, 20)
 white = Color(240, 240, 240)
+
+red_1 = Color(240, 20, 20)
+red_2 = Color(240, 120, 120)
+red_2 = Color(240, 180, 180)
+
+green_1 = Color(20, 240, 20)
+green_2 = Color(120, 240, 120)
+green_3 = Color(180, 240, 180)
+
+blue_1 = Color(20, 20, 240)
+blue_2 = Color(120, 120, 240)
+blue_3 = Color(180, 180, 240)
