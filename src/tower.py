7ca3716ff59bc05db5153b291822e0c8f4c530e2
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-09-21 23:16:10 -0400

    first iteration of tower class

---
+++ src/tower.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+
+class Tower(object):
+
+    def __init__(self, tower_type):
+        self.x = 0
+        self.y = 0
+        self.damage = 1
+        self.level = 1
+        self.type = tower_type
